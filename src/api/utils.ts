f6a04b78d65ffe54a801c41e364acb9e7a40983a
fucloud-win11 <270928474@qq.com>
2024-02-06 16:37:26 +0800

    perfect: define Result, simplify the input param

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,43 +1,60 @@
 import axios from "axios";
+import { plainToInstance } from "class-transformer";
+
+export class Result {
+    data: any;
+    msg: string;
+    code: number;
+
+    constructor(data: any, msg: string, code: number) {
+        this.data = data;
+        this.msg = msg;
+        this.code = code;
+    }
+
+    static from(data: Object) {
+        return plainToInstance(Result, data);
+    }
+}
 
 const basicHeaders = {
     "Content-Type": "application/json",
 };
 
-export function postRequest(apiUrl: string, data: Object) {
+export function postRequest(path: string, data: Object) {
     return axios({
         method: "post",
-        url: apiUrl,
+        url: getApiUrl(path),
         headers: basicHeaders,
         data: data,
-    });
+    }).then((resp) => Result.from(resp.data));
 }
 
-export function deleteRequest(apiUrl: string, data: Object) {
+export function deleteRequest(path: string, data: Object) {
     return axios({
         method: "delete",
-        url: apiUrl,
+        url: getApiUrl(path),
         headers: basicHeaders,
         data: data,
-    });
+    }).then((resp) => Result.from(resp.data));
 }
 
-export function putRequest(apiUrl: string, data: Object) {
+export function putRequest(path: string, data: Object) {
     return axios({
         method: "put",
-        url: apiUrl,
+        url: getApiUrl(path),
         headers: basicHeaders,
         data: data,
-    });
+    }).then((resp) => Result.from(resp.data));
 }
 
-export function getRequest(apiUrl: string, params: Object) {
+export function getRequest(path: string, params: Object) {
     return axios({
         method: "get",
-        url: apiUrl,
+        url: getApiUrl(path),
         headers: basicHeaders,
         params: params,
-    });
+    }).then((resp) => Result.from(resp.data));
 }
 
 export function getApiUrl(name: string) {
