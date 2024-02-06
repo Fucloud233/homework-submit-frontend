ba1b3ddc16ce9d6ca29c6a55337bf6c3fa1ffda6
fucloud-win11 <270928474@qq.com>
2024-02-06 17:37:48 +0800

    refactor: encapsulate the sub layout

---
+++ src/components/layout/Sub.vue
@@ -0,0 +1,30 @@
+<template>
+    <div style="display: flex; flex-direction: row; align-items: center">
+        <el-button
+            @click="$router.back()"
+            style="margin-right: 15px"
+            text
+            circle
+        >
+            <el-icon> <ArrowLeft /> </el-icon>
+        </el-button>
+        <h1>{{ title }}</h1>
+    </div>
+    <el-divider />
+    <slot name="default"></slot>
+</template>
+
+<script lang="ts">
+import { ArrowLeft } from "@element-plus/icons-vue";
+
+export default {
+    props: {
+        title: {
+            type: String,
+        },
+    },
+    components: {
+        ArrowLeft,
+    },
+};
+</script>
