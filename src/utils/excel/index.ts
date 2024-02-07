774eb48ff6b7eec7b2e9038cd96bc93ef5ef4734
fucloud-win11 <270928474@qq.com>
2024-02-07 22:21:30 +0800

    feat: basic excel parser

--- src/utils/excel/index.ts
+++ src/utils/excel/index.ts
@@ -4,22 +4,53 @@ const getValues = (info: any) => {
     return Array.from(Object.values(info));
 };
 
+export const parseCellString = (row: ExcelJS.Row, index: number) => {
+    const cell = row.getCell(index);
+
+    return cell.value != undefined ? cell.value.toString() : undefined;
+};
+
+export const parseCellNumber = (row: ExcelJS.Row, index: number) => {
+    const cell = row.getCell(index);
+
+    return cell.value != undefined
+        ? Number.parseInt(cell.value.toString())
+        : undefined;
+};
+
+export const createBookFromBlob = async (blob: Blob) => {
+    const book = new ExcelJS.Workbook();
+    await book.xlsx.load(await blob.arrayBuffer());
+    return book;
+};
+
 export class SimpleTable {
     book: ExcelJS.Workbook;
-    sheet: ExcelJS.Worksheet;
 
     constructor() {
         this.book = new ExcelJS.Workbook();
-        this.book.addWorksheet();
-        this.sheet = this.book.worksheets[0];
+    }
 
-        // sheet.addRow([1, 2, 3, 4, 5]);
+    static async fromBlob(blob: Blob): Promise<SimpleTable> {
+        const table = new SimpleTable();
+        await table.book.xlsx.load(await blob.arrayBuffer());
+        console.log(table);
+        console.log(table.book.worksheets);
+
+        const sheet = table.book.getWorksheet(1);
+        if (sheet != undefined) {
+            for (let i = 0; i < sheet.rowCount; i++) {
+                console.log(sheet.getRow(i));
+            }
+        }
+
+        return table;
     }
 
     addInfo(info: any) {
         // const values = getValues(info);
         // console.log("values: ", values);
-        this.sheet.addRow(getValues(info));
+        // this.book.getWorksheet(0).addRow(getValues(info));
     }
 
     async toURL() {
@@ -33,11 +64,11 @@ export class SimpleTable {
         return url;
     }
 
-    debug() {
-        console.log(this.sheet.rowCount);
-        const count = this.sheet.rowCount;
-        for (let i = 0; i < count; i++) {
-            console.log(this.sheet.getRow(i + 1));
-        }
-    }
+    // debug() {
+    //     console.log(this.sheet.rowCount);
+    //     const count = this.sheet.rowCount;
+    //     for (let i = 0; i < count; i++) {
+    //         console.log(this.sheet.getRow(i + 1));
+    //     }
+    // }
 }
--- src/utils/excel/student.ts
+++ src/utils/excel/student.ts
@@ -1,5 +1,12 @@
+import * as ExcelJS from "exceljs";
+
 import { Student } from "@/api/student";
-import { SimpleTable } from ".";
+import {
+    SimpleTable,
+    parseCellString,
+    parseCellNumber,
+    createBookFromBlob,
+} from ".";
 
 const chinese: Map<String, String> = new Map<String, String>();
 chinese.set("id", "学号");
@@ -8,25 +15,62 @@ chinese.set("gender", "性别");
 chinese.set("grade", "年级");
 chinese.set("password", "密码");
 
+const mapRowToStudent = (row: ExcelJS.Row) => {
+    return new Student(
+        parseCellNumber(row, 1),
+        parseCellString(row, 2),
+        parseCellNumber(row, 3),
+        parseCellNumber(row, 4),
+        parseCellString(row, 5)
+    );
+};
+
 export class StudentSimpleTable extends SimpleTable {
+    list: Student[] = [];
+
     static header = {};
 
     constructor() {
         super();
+    }
 
-        this.addHeader();
+    static async fromBlob(blob: Blob): Promise<StudentSimpleTable> {
+        let list = [] as Student[];
+
+        const book = await createBookFromBlob(blob);
+
+        console.log(book.worksheets);
+
+        const sheet = book.worksheets[0];
+
+        if (sheet == undefined) {
+        } else {
+            console.log("count: ", sheet.rowCount);
+            const rows = sheet.getRows(2, sheet.rowCount - 1);
+            if (rows != undefined) {
+                list = rows.map(mapRowToStudent);
+            }
+        }
+
+        const table = new StudentSimpleTable();
+        table.list = list;
+        table.book = book;
+
+        console.log(list);
+
+        return table;
     }
 
     // addInfo(info: Student) {
     //     this.sheet.addRow(info);
     // }
 
-    private addHeader() {
-        const columnNames = Array.from(
-            Object.keys(new Student()).map((key) => {
-                chinese.get(key);
-            })
-        );
-        this.sheet.addRow(columnNames);
-    }
+    // private addHeader() {
+    //     const columnNames = Array.from(
+    //         Object.keys(new Student()).map((key) => {
+    //             chinese.get(key);
+    //         })
+    //     );
+    //     this.sheet.addRow(columnNames);
+    // }
 }
